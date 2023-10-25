39ba87f284f87e62337faf3350a6713275dcc13b
unknown <jackone.action.software@gmail.com>
2023-10-25 18:45:19 -0300

    actualizacion de la libreria tools tokenizador | consultas manuales

---
+++ tools/facebook.py
@@ -0,0 +1,26 @@
+def clean_facebook_comments(batch: list) -> list:
+    new_batch = []
+    for comment in batch:
+        
+        # Filtro 1
+        if comment in ["Me gusta", "Responder", "Editado", "Fan destacado"] or comment.isnumeric():
+            continue
+        
+        
+        comment = comment.split(" ")
+        
+        # Filtro 2: registros que tienen patrón "# h" excluídos
+        if len(comment)==2 and comment[1] in ['h', "d", "sem", "respuesta", "respuestas"]:
+            continue
+
+
+        # Filtro 3: eliminar autores
+        # filter_3 = [False if word.istitle() else True for word in comment]
+        filter_3 = [not word.istitle() for word in comment]
+        
+
+
+        if any(filter_3):
+            new_batch.append(" ".join(comment))
+
+    return new_batch
---
+++ tools/tokenizer.py
@@ -0,0 +1,82 @@
+import unicodedata, re
+from typing import List
+
+
+def tratamiento_i(content: str) -> str:
+    """Etapas preprocesamiento:
+    - Eliminación de caracteres no imprimibles
+    - Reemplazo de caracteres por espacios en blanco
+    - Reestructuración de texto para eliminar multiples espacios en blanco
+    """
+    cleaned_content = ''.join(
+        ' ' if c in "&'()*+‘<=>[]^`{|}~ýª!?¿¡.,/⁉️‼:\"-;" else c
+        for c in content
+    )
+    return re.sub("\s+", " ", cleaned_content)
+
+
+
+def eliminacion_tildes(content: str) -> str:
+    return ''.join(
+        unicodedata.normalize('NFD', c)[0]
+        if c in 'áéíóúÁÉÍÓÚáéíóúÁÉÍÓÚó'
+        else c
+        for c in content
+    )
+
+def preprocesamiento(batch: List[str]) -> list:
+    assert isinstance(batch, list), "El argumento batch debe ser una lista"
+    assert isinstance(batch[0], str), "El argumento batch debe ser una lista de strings"
+    
+    batch = [tratamiento_i(content) for content in batch]
+    return batch
+
+def procesamiento(batch: list) -> list:
+    assert isinstance(batch, list), "El argumento batch debe ser una lista"
+    assert isinstance(batch[0], str), "El argumento batch debe ser una lista de strings"
+
+    
+    batch = [eliminacion_tildes(content) for content in batch]
+    batch = [content.lower() for content in batch]
+    
+    return batch
+
+            
+
+
+
+
+# def correccion_de_palabras(content: str) -> str:
+#     "Anulado: no responde como lo eperado"
+#     from spellchecker import SpellChecker
+#     corrector_palabras = SpellChecker(language='es')
+#     correcciones = []
+#     for token in content.split(' '):
+#         # Verificar si la palabra está mal escrita
+#         if corrector_palabras.correction(token) != token:
+#             # Corregir la palabra
+#             corrected_token = corrector_palabras.correction(token)
+#             correcciones.append(corrected_token)
+#         else:
+#             correcciones.append(token)
+#     return correcciones
+
+
+
+
+if __name__ == "__main__":
+    texto = "José Jose joseeee     Patricia! -Bullrich PATRICIA-BULLRICH (patricia) bullrich\
+        !!!!!! Este es otro ejemplo de tokenizadorrrrr tokenizador basado!! en palabras.\
+            Conclusión. Conclusion. conclusion conclusión"
+            
+    print("tratemiento_i")
+    print(tratamiento_i(texto))
+    # print("correccion palabras")
+    # print(correccion_de_palabras(texto))
+    print("preprocesamiento")
+    batch = preprocesamiento([texto])
+    print(batch)
+    
+    print("procesamiento")
+    print(procesamiento(batch))
+    
