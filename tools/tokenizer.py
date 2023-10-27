52e862afc67d38c731dbd0ebfa1da1e17914e6ca
unknown <jackone.action.software@gmail.com>
2023-10-26 21:54:10 -0300

    creacion de features emojis

--- tools/tokenizer.py
+++ tools/tokenizer.py
@@ -1,8 +1,8 @@
 import unicodedata, re
 from typing import List
+import emoji
 
-
-def tratamiento_i(content: str) -> str:
+def eliminacion_caracteres_innecesarios(content: str) -> str:
     """Etapas preprocesamiento:
     - Eliminación de caracteres no imprimibles
     - Reemplazo de caracteres por espacios en blanco
@@ -27,9 +27,8 @@ def eliminacion_tildes(content: str) -> str:
 def preprocesamiento(batch: List[str]) -> list:
     assert isinstance(batch, list), "El argumento batch debe ser una lista"
     assert isinstance(batch[0], str), "El argumento batch debe ser una lista de strings"
-    
-    batch = [tratamiento_i(content) for content in batch]
-    return batch
+
+    return [eliminacion_caracteres_innecesarios(content) for content in batch]
 
 def procesamiento(batch: list) -> list:
     assert isinstance(batch, list), "El argumento batch debe ser una lista"
@@ -42,25 +41,32 @@ def procesamiento(batch: list) -> list:
     return batch
 
             
+def extraccion_emoji(batch: list) -> list:
+    assert isinstance(batch, list), "El argumento batch debe ser una lista"
+    assert isinstance(batch[0], str), "El argumento batch debe ser una lista de strings"
+    
+    return [emoji.replace_emoji(content, "") for content in batch]
 
+def identificar_emoji_list(batch:list) -> list:
+    assert isinstance(batch, list), "El argumento batch debe ser una lista"
+    assert isinstance(batch[0], str), "El argumento batch debe ser una lista de strings"
+    
+    return [emoji.emoji_list(content) for content in batch]
 
-
-
-# def correccion_de_palabras(content: str) -> str:
-#     "Anulado: no responde como lo eperado"
-#     from spellchecker import SpellChecker
-#     corrector_palabras = SpellChecker(language='es')
-#     correcciones = []
-#     for token in content.split(' '):
-#         # Verificar si la palabra está mal escrita
-#         if corrector_palabras.correction(token) != token:
-#             # Corregir la palabra
-#             corrected_token = corrector_palabras.correction(token)
-#             correcciones.append(corrected_token)
-#         else:
-#             correcciones.append(token)
-#     return correcciones
-
+def creacion_emoji_feature(batch:list) -> list:
+    assert isinstance(batch, list), "El argumento batch debe ser una lista"
+    assert isinstance(batch[0], str), "El argumento batch debe ser una lista de strings"
+    
+    emoji_list = identificar_emoji_list(batch)
+    result = []
+    for content in emoji_list:
+        if len(content) == 0:
+            result.append([])
+        else:
+            result.append([match["emoji"] for match in content])
+    
+    return result
+    
 
 
 
@@ -70,7 +76,7 @@ if __name__ == "__main__":
             Conclusión. Conclusion. conclusion conclusión"
             
     print("tratemiento_i")
-    print(tratamiento_i(texto))
+    print(eliminacion_caracteres_innecesarios(texto))
     # print("correccion palabras")
     # print(correccion_de_palabras(texto))
     print("preprocesamiento")
@@ -79,4 +85,26 @@ if __name__ == "__main__":
     
     print("procesamiento")
     print(procesamiento(batch))
+    
+    
+    
+    ##########################
+    batch = [texto, "Hola Mundo!", "bastaaaaaaa", "massa vuelvo pais hijos 🙏 🙏", "🙏🙏🙏"]
+    
+    result = identificar_emoji_list(batch)
+    print(result)
+    feature = creacion_emoji_feature(batch)
+    print(feature)
+    clean_content = extraccion_emoji(batch)
+    print(clean_content)
+    ##########################
+    batch = [texto, "Hola Mundo!", "bastaaaaaaa", "massa vuelvo pais hijos 🙏 🙏", "🙏🙏🙏"]
+    
+    batch = procesamiento(preprocesamiento(batch))
+    
+    content = extraccion_emoji(batch)
+    feature_emoji = creacion_emoji_feature(batch)
+    print(content)
+    print(feature_emoji)
+    
     
