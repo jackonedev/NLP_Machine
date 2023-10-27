2b21e431a2bc2e5abbd6ccd2791e2362012c0988
unknown <jackone.action.software@gmail.com>
2023-10-27 17:33:06 -0300

    ultima version app tokenizadores

--- app/tokenizadores/main.py
+++ app/tokenizadores/main.py
@@ -5,16 +5,19 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..
 sys.path.insert(0, project_root)
 
 
-from app.main.main import tokenizador_i
+from app.main.main import tokenizador
 from tools.feed import crear_directorio, procesar_file_csv
-#import tools
 
-def main(verbose = True):
-    print("Ejecutando tokenizadores/main.py\n")
-    resources = tokenizador_i()
+
+def main(file_name:str=None, verbose = True):
+    if verbose:
+        print("Ejecutando tokenizadores/main.py\n")
+    resources = tokenizador()
     feed = resources["main_feed"]
+    preparation = resources["main_preparation"]
     
-    file_name = input("> nombre archivo: ")
+    if not file_name:
+        file_name = input("> nombre archivo: ")
     if not file_name:
         print("Ejecución interrumpida de forma segura.")
         exit()
@@ -22,10 +25,13 @@ def main(verbose = True):
     nombre, archivo = procesar_file_csv(file_name)
     
     df = feed(archivo)
-    return df, nombre
+    df = preparation(df, quitar=True, token_config=True)
+    df.name = nombre
+    
+    return df
     
 
 
 if __name__ == "__main__":
-    df, nombre = main()
+    df = main()
     df
--- app/tokenizadores/tokenizador_i.py
+++ app/tokenizadores/tokenizador_i.py
@@ -1,9 +1,15 @@
 
+## Librerias Nativas de Python y de Terceros
+try: # < python 3.11
+    import tweetnlp
+except ImportError:
+    pass
 import tiktoken
-import unicodedata
+from tqdm import tqdm
+import pandas as pd
+from datasets import Dataset
 import re
 from nltk.tokenize import word_tokenize
-## Librerias Nativas de Python y de Terceros
 import sys, os, time, pickle
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
@@ -11,10 +17,191 @@ app_root = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 sys.path.insert(0, project_root)
 
 
-from tools.tokenizer import preprocesamiento, procesamiento
+try:
+    from app.tokenizadores.main import main as Main
+except:
+    from main import main as Main
+
+from tools.tokenizer import preprocesamiento, procesamiento, extraccion_emoji, creacion_emoji_feature
 from tools.extraccion_muestras import nombres_propios
 
 #######################################################################
+
+
+
+
+
+
+
+def tokenizador(batch: list) -> pd.DataFrame:
+    def data_stream(samples: Dataset, target:str = 'content'):
+        for i in range(samples.num_rows):
+            yield samples[target][i]
+
+    # CONTENT
+    # texto = "lloviznando lloviznandoo lloviznandooo lloviznandoooo lloviznandooooo"
+    # batch = [texto, "Honnla Maria!", "bastaannaannaaa pedro", "massa vuellvo paiis argentina hijos 🙏 🙏", "🙏🙏🙏"]
+
+
+    # PREPROCESSING
+    batch = preprocesamiento(batch)
+
+    # NER:extracción de nombres propios
+    # 1
+    i_nombres_propios = nombres_propios(batch)
+
+    # 2
+    try:
+        if isinstance(batch, list):
+            data = pd.DataFrame(batch)
+            data.columns = ['content']
+    except AttributeError:
+        assert isinstance(data, pd.DataFrame), "data must be a pandas DataFrame"
+
+    except ValueError:
+        print (" Length mismatch: Expected axis has 5 elements, new values have 1 elements")
+
+    try:
+        data = Dataset.from_pandas(data)
+        model_ner = tweetnlp.load_model('ner')
+        ii_nombres_propios = model_ner.ner(data_stream(data))
+    except NameError:
+        pass
+
+
+    ## EMOJIS  ##
+    feature_emoji = creacion_emoji_feature(batch)
+    batch = extraccion_emoji(batch)
+
+    # PROCESAMIENTO PREVIO A LA TOKENIZACION
+    batch = procesamiento(batch)
+
+    ##  CREACION DEL DATAFRAME DE TRABAJO  ##
+    df = pd.DataFrame({'content': batch, "emojis": feature_emoji , 'i_nombre_propios': i_nombres_propios})
+
+    try:
+        df.loc[:, "ii_nombres_propios"] = ii_nombres_propios
+    except:
+        pass
+
+    # # checkpoint para inspeccionar el dataframe
+    # with open("dftk_oct_unt.pickle", "wb") as f:
+    #     pickle.dump(df, f)
+    print("checkpoint 1: DataFrame previo a la tokenizacion")
+
+
+    ##  TIKTOKEN  ##
+    encoding_base = tiktoken.get_encoding("cl100k_base")
+
+
+
+    batch_tokenizado = [encoding_base.encode(content) for content in batch]
+    batch_decodificado_bytes = [[encoding_base.decode_single_token_bytes(token) for token in content] for content in batch_tokenizado]
+
+    # print(texto_decodificado_bytes)
+    """
+    Notas tokenizador transcripcion.ipynb: 
+        - en el bucle de tratamiento es que se realiza la decodificacion_str
+        - eso era porque algunos caracteres no se podian decodificar
+        - como en este tokenizador ya hemos extraido los emojis, no deberia haber problema
+    """
+    batch_decodificado_str = []
+    for content in batch_decodificado_bytes:
+        
+        
+        # content = [token.decode('UTF-8') for token in content]
+        for i, token in enumerate(content):
+            try:
+                content[i] = token.decode('UTF-8')
+            except:
+                content[i] = " "
+                # quedan caracteres que no se pueden decodificar
+                pass
+                
+        
+        
+        batch_decodificado_str.append(content)
+
+    # Corrección token cero
+    for i, content in enumerate(batch_decodificado_str):
+        if content == []:
+            continue
+        content[0] = " " + content[0]
+        batch_decodificado_str[i] = content
+
+    # texto_decodificado_str = [token.decode('UTF-8') for token in texto_decodificado_bytes]
+
+    # print(batch_decodificado_str)
+
+
+
+
+    #### TRATAMIENTO TOKENIZADOR  ####
+    TOKENS = {} # {0: [tc, ti], 1: [tc, ti], 2: [tc, ti], 3: [tc, ti], ... key_m: [tc, ti]} / key: index, m: cantidad de comentarios -1 (muestra) # porque arranca contando de cero
+
+    for key, content in tqdm(enumerate(batch_decodificado_str), "Tokenizando"):
+
+        tokens_c = [] # hace referencia a tokens_compuestos
+        tokens_i = [] # hace referencia a tokens_independientes
+
+        TOKENS[key] = []
+
+        last_token = ""
+        cache = []
+
+        for ix, token in enumerate(content):
+            
+            if ix == 0:
+                last_token = token
+                continue
+
+            ##  MODULO DE PALABRAS TOKENS COMPUESTOS  ##
+            if not token.startswith(" ") and last_token.startswith(" "):
+                cache.append(last_token)#primer token compuesto
+            elif not token.startswith(" ") and not last_token.startswith(" "):
+                cache.append(last_token)# caso entre medio
+            elif token.startswith(" ") and not last_token.startswith(" "):# and i != 1: # Como corregimos el token 0, es innecesario. esto es para que no se rompa? o porque tiene una función lógica?
+                cache.append(last_token)# caso ultimo token
+                tokens_c.append("".join(cache).strip())
+                cache = []
+                last_token = token
+                continue
+
+            ##  MODULO DE PALABRAS TOKEN INDEPENDIENTE  ##
+            if token.startswith(" ") and last_token.startswith(" "):
+                if len(last_token.strip()) < 3:
+                    last_token = token
+                    continue
+                tokens_i.append(last_token.strip())
+
+            last_token = token
+
+        ## MODULO ULTIMO TOKEN ## #TODO: analizar si last_token debe actualizar a cero junto con la caché
+        if len(cache) > 0:
+            tokens_c.append("".join(cache).strip())
+        # si es un token compuesto lo guardamos, sinó se pierde    
+        # elif len(last_token.strip()) > 0:# la caché es cero y no last_token: el ultimo token es un token compuesto y no se limpia el last_token, por lo tanto el ultimo token del token compuesto se guardaría
+        #     tokens_i.append(last_token.strip())
+
+        TOKENS[key] = [tokens_c, tokens_i]
+
+
+
+
+    print("checkpoint 2: tokenizacion finalizada")
+    # with open("tk_oct_unt.pickle", "wb") as f:
+    #     pickle.dump(TOKENS, f)
+
+    token_feature = pd.DataFrame(TOKENS).T
+    token_feature.columns = ["tokens_i", "tokens_ii"]
+
+    df = pd.concat([df, token_feature], axis=1)
+
+    print("programa finalizado de forma exitosa")
+
+    return df
+
+
 """
 TOKENIZADOR POR TIKTOKEN
 
@@ -41,43 +228,33 @@ ETAPAS TOKENIZADOR:
     -7- aplicar filtro miss-spelling
     
     
+    
+-es verdad que hace falta dos tokenizadores-    
+    
 """;
 
-# CONTENT
-texto = "hola José Jose jose     Patricia -Bullrich PATRICIA-BULLRICH (patricia) bullrich !!!!!! Este es otro ejemplo de tokenizadorrrrr tokenizador basado!! en palabras. Conclusión. Conclusion. conclusion conclusión"
-
-
-# PREPROCESSING
-batch = [texto, "Hola Mundo!", "bastaaaaaaa", "massa vuelvo pais hijos 🙏 🙏"]
-batch = preprocesamiento(batch)
-# extracción de nombres propios
-i_nombre_propios = nombres_propios(batch)
-# eliminación de tildes y pasar todo a minúsculas
-batch = procesamiento(batch)
-
-
-# TIKTOKEN
-encoding_base = tiktoken.get_encoding("cl100k_base")
-
-
-texto_tokenizado = [encoding_base.encode(content) for content in batch]
-texto_decodificado_bytes = [[encoding_base.decode_single_token_bytes(token) for token in content] for content in texto_tokenizado]
-# texto_decodificado_str = [token.decode('UTF-8') for token in texto_decodificado_bytes]
-
-texto_decodificado_bytes
-
-
-
-
-# print("Texto tokenizado de forma numérica I:", texto_tokenizado)
-
-
-# print("Texto decodificado I:", texto_decodificado)
-
-
-
-# print("Texto decodificado II:", texto_decodificado)
-
-
-# print(tuple(zip(texto_tokenizado, texto_decodificado)))
-print("programa finalizado de forma exitosa")
+def main(file_name:str=None, verbose = False):
+    if verbose:
+        print("Ejecutando tokenizadores/tokenizador_i.py\n")
+    
+    df = Main(file_name=file_name, verbose=verbose)
+    nombre = df.name
+    if verbose:
+        print("Saliendo de tokenizadores/main.py\n")
+    
+    assert "content" in df.columns, "df debe tener una columna content"
+    
+    batch = df["content"].tolist()
+    
+    dftk = tokenizador(batch)
+    
+    
+    df["content"] = dftk["content"]
+    dftk = dftk.drop("content", axis=1)
+    
+    
+    features = ["content", "usuarios_mencionados", "hashtags"]
+    
+    
+    return pd.concat([df[features], dftk], axis=1)
+    
