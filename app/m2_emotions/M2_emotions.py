8aad956137a615228f6374758de518e19bdf523e
unknown <jackone.action.software@gmail.com>
2023-10-31 08:21:03 -0300

    added funcion main_df

--- app/m2_emotions/M2_emotions.py
+++ app/m2_emotions/M2_emotions.py
@@ -1,4 +1,6 @@
 import sys, os
+import torch
+import time
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 app_root = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
@@ -42,7 +44,7 @@ def data_stream(samples: Dataset, target:str = 'content'):
 
 
 # classifier function with batching option
-def classify_tweets(model:pipeline, data:pd.DataFrame, target:str = "content", batching:bool = True) -> list:
+def classify_tweets(model:pipeline, data:pd.DataFrame, target:str = "content", batching:bool = False) -> list:
     """
     Classify tweets based on given targets and labels using a HuggingFace pipeline.
 
@@ -98,6 +100,79 @@ def classify_tweets(model:pipeline, data:pd.DataFrame, target:str = "content", b
     
 
     return res
+
+def emotions_features(predictions:list) -> pd.DataFrame:
+    # Crear una lista para almacenar los datos de cada columna
+    label_columns = []
+    score_columns = []
+    max_label_column = []
+
+    # Recorrer la lista de predicciones
+    for prediction in predictions:
+        labels = []
+        scores = []
+        max_score = 0
+        max_label = ''
+
+        # Recorrer cada predicción dentro de la lista
+        for pred in prediction:
+            label = pred['label']
+            score = pred['score']
+
+            labels.append(label)
+            scores.append(score)
+
+            # Actualizar la etiqueta con el mayor puntaje
+            if score > max_score:
+                max_score = score
+                max_label = label
+
+        label_columns.append(labels)
+        score_columns.append(scores)
+        max_label_column.append(max_label)
+
+    # Crear el DataFrame con las columnas correspondientes
+    df = pd.DataFrame(
+    {
+        'emotions_6_max_label': max_label_column,
+        'emotions_6_labels': label_columns,
+        'emotions_6_scores': score_columns
+        }
+    )
+
+    return df
+
+def main_df(df: pd.DataFrame, verbose:bool=False) -> pd.DataFrame:
+
+    start = time.time()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    MODEL = "02shanky/finetuned-twitter-xlm-roberta-base-emotion"
+    tokenizer_i = AutoTokenizer.from_pretrained(MODEL)
+    model_i = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+    pipeline_i = pipeline('text-classification', model=model_i, tokenizer=tokenizer_i, device=device, top_k=None)
+
+    # Realizar predicción y ajustar resultados
+    predictions = classify_tweets(pipeline_i, df, target="content")
+
+    predictions = emotions_features(predictions)
+
+    # update dataset
+    # df = pd.concat([df, predictions], axis=1)
+    
+    end = time.time()
+    if verbose:
+        print(f"tiempo de ejecución: {end - start} segs")
+    
+    return predictions
+
+
+
+
+
+
+
 
 
 
